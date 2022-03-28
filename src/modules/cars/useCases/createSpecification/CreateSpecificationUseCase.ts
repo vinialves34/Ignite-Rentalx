ce467a4bd8dd96a3ac1a8b319b442d15e59b3ed1
Vinicius Alves <viniciusklonny@hotmail.com>
2022-03-28 18:56:56 -0300

    Refactor Specification File

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,23 @@
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificationsRespository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationsRespository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification Already Exists!");
+    }
+
+    this.specificationsRespository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationUseCase };
