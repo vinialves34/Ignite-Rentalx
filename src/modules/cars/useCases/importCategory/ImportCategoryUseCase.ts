0ea483f74abac3cfc7f5fcdd4ca85ba765af60a5
Vinicius Alves <viniciusklonny@hotmail.com>
2022-03-28 19:46:21 -0300

    Create ImportCategory

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+import { parse as csvParse } from "csv-parse";
+
+class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    })
+  }
+}
+
+export { ImportCategoryUseCase };
