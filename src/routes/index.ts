4bb9de69d4b5cf06310091f442687e0f4ccd7818
Vinicius Alves <vinialves.dev@gmail.com>
2022-07-21 15:58:37 -0300

    Create alias for folders

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
-import { specificationsRoutes } from "./Specifications.routes";
+import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 
 const router = Router();
