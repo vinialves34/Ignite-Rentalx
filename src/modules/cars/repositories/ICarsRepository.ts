9a1489ffb0aa429c33c679eb01af414f002431df
Vinicius Alves <vinialves.dev@gmail.com>
2022-09-18 14:35:53 -0300

    Create useCase to cars

---
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -0,0 +1,9 @@
+import { ICreateCarDTO } from "../dto/ICreateCarDTO";
+import { Car } from "../infra/typeorm/entities/Car";
+
+interface ICarsRepository {
+  create(data: ICreateCarDTO): Promise<Car>;
+  findByLicensePlate(license_plate: string): Promise<Car>;
+}
+
+export { ICarsRepository };
